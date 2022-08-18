be53e236afe10f8d215dd71bb85b1d2ded8287a5
CrucianFrik <95472134+CrucianFrik@users.noreply.github.com>
2022-08-18 13:33:45 +0300

    logs task added

--- AutoPilot.ino
+++ AutoPilot.ino
@@ -4,12 +4,16 @@
 
 #define DATA_UPT_TASK_PERIOD  1 //millis
 #define CONTROL_TASK_PERIOD   1 //millis
+#define LOG_TASK_PERIOD   10 //millis
 
 #define borders(Max, Min, val) (val > Max)? Max : (val < Min)? Min : val
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
+TaskHandle_t Task3;
 SemaphoreHandle_t xBinarySemaphore;
+
+String filestr;
 
 void control(void* pvParameters){
   portTickType xLastWakeTime;
@@ -17,6 +21,15 @@ void control(void* pvParameters){
   while(true){
     vTaskDelayUntil( &xLastWakeTime, ( CONTROL_TASK_PERIOD / portTICK_RATE_MS ) );
   }
+}
+
+void logs(void* pvParameters){
+  if (xSemaphoreTake(xBinarySemaphore, portMAX_DELAY) == pdPASS) {
+    sd_write(filestr, String(roll)+" "+String(pitch)+"\n");
+    Serial.println(String(pitch) + " " + String(roll));
+    xSemaphoreGive(xBinarySemaphore);
+  }    
+  vTaskDelayUntil( &xLastWakeTime, ( LOG_TASK_PERIOD / portTICK_RATE_MS ) );
 }
 
 void data_update(void* pvParameters){
@@ -29,23 +42,29 @@ void data_update(void* pvParameters){
         roll = angles.x;
         pitch = angles.y;
         yaw = angles.z;
-            Serial.println(String(pitch) + " " + String(roll));
-            xSemaphoreGive(xBinarySemaphore);
+        xSemaphoreGive(xBinarySemaphore);
         }       
    }
-    vTaskDelayUntil( &xLastWakeTime, ( DATA_UPT_TASK_PERIOD / portTICK_RATE_MS ) );
+   vTaskDelayUntil( &xLastWakeTime, ( DATA_UPT_TASK_PERIOD / portTICK_RATE_MS ) );
   }
 }
 
 void setup() {
   Serial.begin(115200);
+  EEPROM.begin(EEPROM_SIZE);
+  filenumber = EEPROM.read(0);
+  if ((filenumber<0) or (filenumber>256)){ filenumber=0; }
+  filestr="/log_"+String(filenumber)+".txt";
 
   init_mpu9250();
+  sd_init();
   
   xBinarySemaphore = xSemaphoreCreateBinary();
   xTaskCreatePinnedToCore(data_update,"data_update",10000,NULL,10,&Task2,0);
   delay(5000);
   xTaskCreatePinnedToCore(control,"control",10000,NULL,10,&Task1,1); 
+  delay(500);
+  xTaskCreatePinnedToCore(logs,"logs",10000,NULL,9,&Task3,1); 
   delay(500);
   xSemaphoreGive(xBinarySemaphore); 
 }
